20f3359180ab62c9c316af286028b4120c57e2a6
VTwin90 <vanja.torp1@gmail.com>
2021-06-20 17:04:30 +0000

    add function startgame

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+const textElement = document.getElementById('text');
+const optionButtonsElement = document.getElementById('option-buttons');
+
+let state = {};
+
+function startGame() {
+  state = {};
+  showTextNode(1);
+}
+
