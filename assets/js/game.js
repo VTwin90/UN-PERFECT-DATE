a1a8ed3e4e73e808abce62fd7d4c9ee1e345234a
VTwin90 <vanja.torp1@gmail.com>
2021-06-20 17:05:55 +0000

    add function showTextNode

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,3 +8,21 @@ function startGame() {
   showTextNode(1);
 }
 
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex);
+  textElement.innerText = textNode.text;
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild);
+  }
+
+  textNode.options.forEach(option => {
+    if (showOption(option)) {
+      const button = document.createElement('button');
+      button.innerText = option.text;
+      button.classList.add('btn');
+      button.addEventListener('click', () => selectOption(option));
+      optionButtonsElement.appendChild(button);
+    }
+  });
+}
+
