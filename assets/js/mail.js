d37ec625d4be28d19af79aa86801f6fe746ef705
VTwin90 <vanja.torp1@gmail.com>
2021-06-24 15:12:43 +0000

    update template id

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("service_gmail", "UnPerfectDate", {
+    emailjs.send("service_gmail", "template_a9qjkmq", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "message": contactForm.message.value
