c1fae1b6e5abc9994a6d8c44cb45da1edda9602e
VTwin90 <vanja.torp1@gmail.com>
2021-06-26 16:45:42 +0000

    update mail.js

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,15 +1,15 @@
 function sendMail(contactForm) {
-    emailjs.send("service_gmail", "template_a9qjkmq", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "message": contactForm.message.value
+    emailjs.send('service_gmail', 'template_a9qjkmq', {
+        'from_name': contactForm.name.value,
+        'from_email': contactForm.emailaddress.value,
+        'message': contactForm.message.value
     })
     .then(
         function(response) {
-            console.log("SUCCESS", response);
+            console.log('SUCCESS', response);
         },
         function(error) {
-            console.log("FAILED", error);
+            console.log('FAILED', error);
         }
     );
     return false;  // To block from loading a new page
